c879e0c5db1ee5a5fdee594e48526878cb9482b5
Nico Farias <nicolas.farias@sansano.usm.cl>
2021-06-15 01:00:20 -0400

    Se creó Reponder Post Activity

---
+++ wufker-app-final/app/src/main/java/com/messirvedevs/wufker/ResponderPostActivity.java
@@ -0,0 +1,89 @@
+package com.messirvedevs.wufker;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.messirvedevs.wufker.databinding.ActivityResponderPostBinding;
+
+public class ResponderPostActivity extends AppCompatActivity {
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityResponderPostBinding binding;
+
+    private String category, postTitle, postContent;
+    private String answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityResponderPostBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        Bundle bundle = getIntent().getExtras();
+        category = bundle.getString("category");
+        postTitle = bundle.getString("postTitle");
+        postContent = bundle.getString("postContent");
+
+        TextView cat_txtview = findViewById(R.id.category);
+        TextView title_txtview = findViewById(R.id.postTitle);
+        TextView content_txtview = findViewById(R.id.postContent);
+        cat_txtview.setText(category);
+        title_txtview.setText(postTitle);
+        content_txtview.setText(postContent);
+    }
+
+    public void responder(View view) {
+        TextInputEditText editAnswer = findViewById(R.id.answer);
+        answer = editAnswer.getText().toString().trim();
+
+        if (answer.length() > 0) {
+            // Agregar la respuesta a firebase
+
+        } else {
+            Toast.makeText(this, "Los campos no pueden estar vacíos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void cancelar(View view) {
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
