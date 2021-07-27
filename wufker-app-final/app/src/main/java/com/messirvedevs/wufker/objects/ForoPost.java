dd768bb77d9f5b8edb196fca773a3b5fc3dbbb50
bsolar.v <bastian.solar.v@gmail.com>
2021-07-27 08:22:34 -0400

    profiles redirect

--- wufker-app-final/app/src/main/java/com/messirvedevs/wufker/objects/ForoPost.java
+++ wufker-app-final/app/src/main/java/com/messirvedevs/wufker/objects/ForoPost.java
@@ -1,22 +1,26 @@
 package com.messirvedevs.wufker.objects;
 
+import java.util.Date;
+
 public class ForoPost {
     private String id;
     private String category;
     private String title;
     private String authorEmail;
     private String content;
+    private Date timestamp;
 
 
     public ForoPost() {
     }
 
-    public ForoPost(String category, String title, String authorEmail, String content, String id) {
+    public ForoPost(String category, String title, String authorEmail, String content, String id, Date timestamp) {
         this.category = category;
         this.title = title;
         this.authorEmail = authorEmail;
         this.content = content;
         this.id = id;
+        this.timestamp = timestamp;
     }
 
     public String getId() {
@@ -56,5 +60,17 @@ public class ForoPost {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
     }
 }
